3ed5483f825c21bb0a7f61945687c427cb2420b3
sjb351 <115476852+sjb351@users.noreply.github.com>
2023-12-20 16:01:24 +0000

    changes

--- test.py
+++
@@ -1,7 +0,0 @@
-from freppleAPImodule import freppleConnect
-
-frepple = freppleConnect("admin", "admin", "http://129.169.48.176:9000")
-
-outNotConfirmend = frepple.findAllPurchaseOrdersOrd("RL100100", "proposed")
-
-print(outNotConfirmend)
